88017e257f17023e6586ad7f86f83ed511352fa2
Abhishek Kumar Thakur <abhishekkrt27@gmail.com>
2025-12-06 15:48:57 +0530

    Add files via upload

---
+++ ASCII.cpp
@@ -0,0 +1,12 @@
+//8. Program to find ASCII value of a character.
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    char c;
+    cout<<"Enter any character:";
+    cin>>c;
+    cout<<"ASCII Value is : "<<int(c);
+    return 0;
+}
