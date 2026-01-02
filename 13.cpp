0b7a705d34ddb3cebf47dfae648b1f75d81de6e0
Abhishek Kumar Thakur <abhishekkrt27@gmail.com>
2026-01-02 13:15:14 +0530

    Add files via upload

---
+++ 13.cpp
@@ -0,0 +1,15 @@
+//13. Program to demonstrate logical operators.
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int p = 0, q = 1;
+    
+    cout << "p && q: " << (p && q) << '\n';
+    cout << "p || q: " << (p || q) << '\n';
+    cout << "!p: " << (!p) << '\n';
+   
+    return 0;
+}
