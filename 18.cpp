19c066d37b2444ceedc8e3b95aab40aee0b3c2df
Abhishek Kumar Thakur <abhishekkrt27@gmail.com>
2026-01-02 14:43:35 +0530

    Add files via upload

---
+++ 18.cpp
@@ -0,0 +1,15 @@
+//18. Program to evaluate an algebraic expression.
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a=10,b=12;
+    int c;
+
+    c=(a+b)/2;
+    cout<<"Evaluate (a+b)/2:"<<c<<endl;
+
+    return 0;
+}
