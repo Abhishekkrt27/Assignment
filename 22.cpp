fa74a9f8ff3790ff09a5f8f692ac78a97d053037
Abhishek Kumar Thakur <abhishekkrt27@gmail.com>
2026-01-02 20:47:50 +0530

    Add files via upload

---
+++ 22.cpp
@@ -0,0 +1,18 @@
+//22. Program using if-else.
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (num % 2 == 0)
+        cout <<num << " Even number";
+    else
+        cout <<num<< " Odd number";
+
+    return 0;
+}
