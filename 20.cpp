c4aa8bf16fec0bb26f0a446c817ae170da4c48f1
Abhishek Kumar Thakur <abhishekkrt27@gmail.com>
2026-01-02 14:49:02 +0530

    Add files via upload

---
+++ 20.cpp
@@ -0,0 +1,24 @@
+//20. Program to check if a number is positive/negative/zero.
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int num;
+    cout << "Enter a number: ";
+    cin>>num;
+    if (num > 0)
+    {
+        cout << "Positive\n";
+    }
+    else if (num < 0)
+    {
+        cout << "Negative\n";
+    }
+    else
+    {
+        cout << "Zero\n";
+    }
+    return 0;
+}
