84d302904b8ad86bcec2213cf0e47b2029335943
Abhishek Kumar Thakur <abhishekkrt27@gmail.com>
2026-01-13 19:41:33 +0530

    Add files via upload

---
+++ 5.cpp
@@ -0,0 +1,14 @@
+//5. Program using const keyword.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    const int x = 10;   
+
+    cout << "Value of x = " << x;
+
+    // x = 20;  cannot change value of const variable
+
+    return 0;
+}
