b87a4b4335da7b456c5b587b89ce87dac9faee22
Abhishek Kumar Thakur <abhishekkrt27@gmail.com>
2026-01-13 22:48:31 +0530

    Update 10.cpp

--- 10.cpp
+++ 10.cpp
@@ -8,8 +8,8 @@ int main()
     float a = 10.5;
     int b;
     b = (int)a;  
-    cout << "Integer value = " << a << endl;
-    cout << "Float value = " << b << endl;
-
+    cout << "Integer value=" << a << endl;
+    cout << "Float value=" << b << endl;
     return 0;
 }
+
