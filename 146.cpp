34aacd11d8a68ec5d74cfee021bde9082b4641ff
Abhishek Kumar Thakur <abhishekkrt27@gmail.com>
2026-01-13 14:36:58 +0530

    Add files via upload

---
+++ 146.cpp
@@ -0,0 +1,23 @@
+//146. Program using try-catch block.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    try {
+        if (b == 0) {
+            throw b;  
+        }
+        cout << "Result = " << a / b << endl;
+    }
+    catch (int) {
+        cout << "Error: Division by zero is not allowed";
+    }
+
+    return 0;
+}
