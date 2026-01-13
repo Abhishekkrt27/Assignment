b9ff5f5265e93e07c21d8a14a4aa31222ca944f7
Abhishek Kumar Thakur <abhishekkrt27@gmail.com>
2026-01-13 19:50:35 +0530

    Add files via upload

---
+++ 10.cpp
@@ -0,0 +1,15 @@
+//10. Program to demonstrate type casting.
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    float a = 10.5;
+    int b;
+    b = (int)a;  
+    cout << "Integer value = " << a << endl;
+    cout << "Float value = " << b << endl;
+
+    return 0;
+}
